38940dabef868ea3f0129e2a96bfce9fa3c5c9d8
Gabriel Vieira Vidal <gabrielvieriavidal2005@gmail.com>
2024-01-01 20:56:17 -0300

    Features:
    -Seção sobre mim completa
    
    -Sombra da barra de navegação retirada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
 export default function Home() {
   return(
     <div>
-      <p>Home</p>
+      <div className="aboutContainer">
+        <div className="partOne">
+          <h2 className="titleAbout">Olá, meu nome é Gabriel!</h2>
+          <p className="descAbout">Sou um apaixonado por programação. Meu foco é desenvolver soluções inteligentes para problemas difíceis.</p>
+        </div>
+
+        <div className="partTwo"></div>
+      </div>
+
+      <div className="skillContainer">
+        
+      </div>
     </div>
   )
 }
